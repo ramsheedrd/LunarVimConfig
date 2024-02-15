8163ddff0aa7bd58517f7abcbd54981c70e39349
ramsheed <ramsheed@hamon.in>
2024-02-15 12:59:09 +0530

    Modifies copilot shortcut

--- config.lua
+++ config.lua
@@ -278,6 +278,17 @@ lvim.plugins = {
   },
   {
     "github/copilot.vim",
+    init = function()
+      vim.g.copilot_no_tab_map = true
+    end,
+    config = function()
+      vim.keymap.set('i', '<C-w>', [[copilot#Accept("\<CR>")]], {
+        silent = true,
+        expr = true,
+        script = true,
+        replace_keycodes = false,
+      })
+    end,
   },
   {
     "stevearc/oil.nvim",
